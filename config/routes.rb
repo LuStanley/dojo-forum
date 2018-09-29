407df2dfb4bdbf5ea8f365cd0bcd4af019b427c1
Stanley <Stanley741025@gmail.com>
2018-09-30 01:24:32 +0800

    set up article resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :articles
   root "articles#index"
 
   namespace :admin do
