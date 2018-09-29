a63b6dda04ab27542b05acb50ac926f3ffb1f741
Stanley <Stanley741025@gmail.com>
2018-09-30 05:06:29 +0800

    set up article edit action and view for public users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show]
+  before_action :set_article, only: [:show, :edit]
 
   def index
     @articles = Article.page(params[:page]).per(20)
@@ -22,7 +22,7 @@ class ArticlesController < ApplicationController
     end
   end
 
-  
+
 
 
   private
