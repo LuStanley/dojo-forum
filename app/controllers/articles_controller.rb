044a95deedbf1574d6f14880c07f7c625986cc66
Stanley <Stanley741025@gmail.com>
2018-09-30 03:00:02 +0800

    set up article create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,24 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    @article = Article.new(article_params)
+    @article.user_id = current_user.id
+
+    if @article.save
+      flash[:notice] = "Sucessfully post a article"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "Post fail"
+      render :new
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content, :image, :role, :user_id, :category_id)
+  end
+
 end
