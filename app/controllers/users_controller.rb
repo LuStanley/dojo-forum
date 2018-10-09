34e41ed219800c8a870d35cc6ab0c15f9a83bd02
Stanley <Stanley741025@gmail.com>
2018-10-09 20:28:46 +0800

    set up user edit and update action and view on profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,24 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
-    def show
-      @user = User.find(params[:id])
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "User profile was successfully updated"
+      redirect_to user_path(@user)
+    else
+      flash.now[:alert] = "User profile was failed to update"
+      render :edit
     end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :intro, :avatar)
+  end
+
 end
