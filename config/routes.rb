8d46f53f4a6a89c4b2a9f4fbf6b63b6d412261db
Stanley <Stanley741025@gmail.com>
2018-10-09 19:41:22 +0800

    set up user routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :articles
   root "articles#index"
 
+  resources :users, only: [:show, :edit, :update]
+
   namespace :admin do
     root "articles#index"
   end
