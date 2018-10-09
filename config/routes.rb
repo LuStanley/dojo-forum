59ab1754d044ad7ac24c78a9a9ca1b330c790649
Stanley <Stanley741025@gmail.com>
2018-10-09 21:08:19 +0800

    set up comment article for public users

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
-  root "articles#index"
+  resources :articles do
+    resources :comments, only: [:create, :destroy]
+  end
 
   resources :users, only: [:show, :edit, :update]
+  root "articles#index"
 
   namespace :admin do
     root "articles#index"
